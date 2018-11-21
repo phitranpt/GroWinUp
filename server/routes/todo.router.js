13258d686922904cb447a44b862d2a78f25221e6
phitranpt <phi.tran.pt@gmail.com>
2018-11-21 15:45:18 -0600

    fixed two id warning issue. changed it from person.id to task.id instead, now each key will be unique

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //GET all todo task router
 router.get('/:id', (req, res) => {
     const personId = req.params.id;
-    const sqlText = `SELECT person.id, task.task_name FROM person
+    const sqlText = `SELECT task.id, person.username, task.task_name, user_task.completed FROM person
                      JOIN admin_user ON admin_user.user_id = person.id
                      JOIN user_task ON user_task.user_id = admin_user.user_id
                      JOIN task ON task.id = user_task.task_id
