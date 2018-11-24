9f38e3554a7d020096523b1257436720ab89415b
phitranpt <phi.tran.pt@gmail.com>
2018-11-24 14:33:09 -0600

    updated add user page. user is now able to select if admin or child, and avatar

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -16,11 +16,14 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
 router.post('/register', (req, res, next) => {  
+  console.log('in register', req.body);
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const profile_image = req.body.profile_image;
+  const admin = req.body.admin;
 
-  const queryText = 'INSERT INTO person (username, password) VALUES ($1, $2) RETURNING id';
-  pool.query(queryText, [username, password])
+  const queryText = 'INSERT INTO person (username, password, profile_image, admin) VALUES ($1, $2, $3, $4) RETURNING id';
+  pool.query(queryText, [username, password, profile_image, admin])
     .then(() => { res.sendStatus(201); })
     .catch((err) => { next(err); });
 });
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { TextField } from '@material-ui/core';
+
+import '../Style/Style.css';
+
+import { TextField, FormControlLabel } from '@material-ui/core';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
 
 class AddUser extends Component {
 
@@ -40,6 +45,7 @@ class AddUser extends Component {
                     onChange={this.handleChange}
                     margin="normal"
                     name="username"
+                    type="text"
                     /><br></br>
                      <TextField 
                     label="Create New Password"
@@ -47,7 +53,31 @@ class AddUser extends Component {
                     onChange={this.handleChange}
                     margin="normal"
                     name="password"
+                    type="password"
                     /><br></br>
+
+                    <RadioGroup
+                    value={this.state.profile_image}
+                    onChange={this.handleChange}
+                    name="profile_image"
+                    className="adminAvatar"
+                    >
+                    <FormControlLabel value="1" control={<Radio />} label="Blue Fish" />
+                    <FormControlLabel value="2" control={<Radio />} label="Crab" />
+                    <FormControlLabel value="3" control={<Radio />} label="Green Fish" />
+                    <FormControlLabel value="4" control={<Radio />} label="Owl" />
+                    <FormControlLabel value="5" control={<Radio />} label="Vulture" />
+                    </RadioGroup>
+
+                    <RadioGroup
+                    value={this.state.admin}
+                    onChange={this.handleChange}
+                    name="admin"
+                    className="adminRadio"
+                    >
+                    <FormControlLabel value="true" control={<Radio />} label="Admin" />
+                    <FormControlLabel value="false" control={<Radio />} label="Child" />
+                    </RadioGroup>
                     <input type="submit"/>
                 </form>
             </div>
